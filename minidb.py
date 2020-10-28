7003b9ed460381cfaf2858686a1d9b86cf3714aa
ali-04 <mahmohamad560@gmail.com>
2020-10-28 21:30:34 +0330

    add code

--- minidb.py
+++ minidb.py
@@ -1,13 +1,54 @@
-def dbc_open (loc):
-    from csv import reader
-    o = open (loc,'r')
-    dic = dict()
-    with o as p:
-        fil = list(reader(p))
-        for q in fil :
-            dic[q[0]] = q[1:]
-    o.close()
-    return dic
 
+
+def dbc_open (loc):
+    
+    from os import path
+    from csv import reader
+
+    if path.exists(loc):
+        o = open (loc,'r')
+        with o as p:
+            fil = list(reader(p))
+            o.close()
+        return fil
+    else:
+        q = open (loc,'w+')
+        q.close()
+
+        return("it's first use")
+
+
+
+#           |-------\           \      /            |-------
+#           |        \           \    /             |
+#           |        /            \  /              |
+#           |-------/              \/               |-------
+#           |       \              |                |
+#           |        \             |                |
+#           |        /             |                |
+#           |-------/              |                |-------
+
+
+
+def dbc_write (loc,lis):
+    with open(loc,'w+') as File:
+
+        for line in lis:
+            q = ''
+            
+            
+            for har_chiz in line:
+                q += ( str(har_chiz) + ',')
+            File.write(q[: len(q)-1 ])
+            
+            
+            File.write('\n')
+        
+        File.close()
+
+
+
+#dbc_write("/home/mohammad/Documents/GitHub/python/1.csv",[[1],['w',5]])
 
 #print (dbc_open("D:\e.csv"))
+
