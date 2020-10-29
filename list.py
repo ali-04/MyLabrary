d4cc2fb350cce626d59c6f501f425904ef1477b3
ali-04 <mahmohamad560@gmail.com>
2020-10-29 17:07:36 +0330

    Compelet db and debug list's library

---
+++ list.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+def list_get (lis,x,y):
+    if len(lis) > x :
+        return lis[x]
+    else:
+        return y
+
+
+
+
+def blist (lis,x):
+    if len(lis) > x :
+        return True 
+    else:
+        return False 
+
+
+
+
+
+def alist (lis,x,y):
+    for q in range (0, x - len(lis) +1 ):
+        lis.append (y)
+
+
+
+
+
+#ew  = [1,2,3,5,7]
+#alist(ew ,10,int())
+
+#qqq = 0
+
+
--- minidb.py
+++ minidb.py
@@ -1,22 +1,42 @@
 
 
+
+Rr = lambda x : ','.join(x)
+
+
+
+
+
+
+
+def blist (lis,x):
+    if len(lis) > x :
+        return True 
+    else:
+        return False 
+
+
+
+def alist (lis,x,y):
+    for q in range (0, x - len(lis) +1 ):
+        lis.append (y)
+
+
+
+
+
 def dbc_open (loc):
     
-    from os import path
+    #    from os import path
     from csv import reader
 
-    if path.exists(loc):
-        o = open (loc,'r')
-        with o as p:
-            fil = list(reader(p))
-            o.close()
-        return fil
-    else:
-        q = open (loc,'w+')
-        q.close()
-
-        return("it's first use")
-
+    
+    o = open (loc,'r')
+    with o as p:
+        fil = list(reader(p))
+        o.close()
+    return fil
+    
 
 
 #           |-------\           \      /            |-------
@@ -34,18 +54,64 @@ def dbc_write (loc,lis):
     with open(loc,'w+') as File:
 
         for line in lis:
-            q = ''
+            q = list()
             
             
             for har_chiz in line:
-                q += ( str(har_chiz) + ',')
-            File.write(q[: len(q)-1 ])
+                q.append ( str(har_chiz) )
+            File.write(Rr(q))
             
             
             File.write('\n')
         
         File.close()
 
+
+
+
+
+
+
+
+def dbc (name,code) :
+    
+    from os import path
+    from os import getcwd
+
+    loc = str( getcwd() ) + '\\' + ('%s.csv' %name)
+
+    if not ( path.exists(  loc  ) ) :
+        q2 = open (loc,'w+')
+        q2.close 
+
+
+    if type(code) == str :
+        if code == 'open':    
+            return dbc_open(loc)
+
+
+    elif type(code) == list :
+        liscode = code[1:]
+        code = code[0]
+            
+
+        if code == 'edit' :
+                
+            fFile = dbc_open(loc)
+            for q in liscode:
+
+                if not ( blist(fFile , int(  q[0]  ) ) ):
+                    alist ( fFile , int(  q[0]  ) , str() )
+
+
+                fFile [ int(  q[0]  ) ] = q[1:]
+                dbc_write(loc,fFile)
+
+
+
+
+
+#dbc('q',['edit',['2','9']])
 
 
 #dbc_write("/home/mohammad/Documents/GitHub/python/1.csv",[[1],['w',5]])
